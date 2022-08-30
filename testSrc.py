1f0aac6ea6ca6826dd861d382856586ec4b31c5f
jaqenwang <jaqen@outlook.com>
2022-08-30 21:59:52 +0800

    1

--- testSrc.py
+++ testSrc.py
@@ -1,9 +1,11 @@
 import sys
 
-def func(argv):
-    with open(argv,"w") as f:
-        f.write("Hello World!")
+#def func(argv):
+    #with open(argv,"w") as f:
+    #    f.write("Hello World!")
+
 
 if __name__ == '__main__':
     if(len(sys.argv)>1):
-        func(sys.argv[1])
+        #func(sys.argv[1])
+        print(sys.argv[1:])
