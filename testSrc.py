93c8874992f0d629829fa6ff9201b5da291d01b4
jaqenwang <jaqen@outlook.com>
2022-08-31 18:40:57 +0800

    Update testSrc.py

--- testSrc.py
+++ testSrc.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 #def func(argv):
     #with open(argv,"w") as f:
@@ -9,3 +10,4 @@ if __name__ == '__main__':
     if(len(sys.argv)>1):
         #func(sys.argv[1])
         print(sys.argv[1:])
+    print(os.stat(r'\\192.168.88.253\bk\whui\1.txt').st_size)
